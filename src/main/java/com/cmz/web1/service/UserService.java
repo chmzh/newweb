8e3eda6322fe5a2eba3b5d2f6419d12532c72810
chenmingzhou <sd@dfh.com>
2016-11-24 16:45:23 +0800

    定时器

--- src/main/java/com/cmz/web1/service/UserService.java
+++ src/main/java/com/cmz/web1/service/UserService.java
@@ -3,12 +3,15 @@ package com.cmz.web1.service;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cmz.web1.dao.UserDao;
 
 @Service
+@EnableScheduling
 public class UserService {
 	
 	@PostConstruct
@@ -23,5 +26,10 @@ public class UserService {
 	public void update(){
 		int r = userDao.update("abcaa", 9);
 		int c = userDao.count("user");
+	}
+	
+	@Scheduled(cron="* * * * * ?")
+	public void startPayment(){
+		System.out.println("startPayment");
 	}
 }
