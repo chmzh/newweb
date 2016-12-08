3d187e772f9c327025cfc00114e7014985af3713
chenmingzhou <sd@dfh.com>
2016-12-08 17:56:52 +0800

    aouth2

--- src/main/java/com/cmz/web1/oauth/client/OAuth2Client.java
+++ src/main/java/com/cmz/web1/oauth/client/OAuth2Client.java
@@ -1,5 +1,28 @@
 package com.cmz.web1.oauth.client;
 
-public class OAuth2Client {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+
+public class OAuth2Client {
+	public static void main(String[] args) {
+		ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();		
+//		List<String> scopes = new ArrayList<String>(2);
+//        scopes.add("write");
+//        scopes.add("read");
+        resource.setAccessTokenUri("http://localhost:8080/newweb/oauth/token");
+        resource.setClientId("my-trusted-client");
+        //resource.setClientSecret("restapp");
+        resource.setGrantType("password");
+        //resource.setScope(scopes);
+
+        resource.setUsername("marissa");
+        resource.setPassword("koala");
+		OAuth2RestTemplate template = new OAuth2RestTemplate(resource);
+		OAuth2AccessToken token = template.getAccessToken();
+		System.out.println(token);
+	}
 }
