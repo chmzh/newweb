00cd01b1f3182528c20c42456133b79169cb4860
chenmingzhou <sd@dfh.com>
2016-12-15 11:27:33 +0800

    u

--- src/main/java/com/cmz/web1/controller/IonicController.java
+++ src/main/java/com/cmz/web1/controller/IonicController.java
@@ -1,6 +1,7 @@
 package com.cmz.web1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +13,8 @@ public class IonicController {
 	}
 	
 	@RequestMapping("form")
-	public String form(){
+	public String form(Model model){
+		model.addAttribute("content", "你好");
 		return "ionic_form";
 	}
 }
