bfb15e44c042d9361b3a9eb593c886b09ac0f707
chenmingzhou <sd@dfh.com>
2016-12-20 14:24:54 +0800

    joda time

--- src/main/java/com/cmz/web1/util/DateTimeUtil.java
+++ src/main/java/com/cmz/web1/util/DateTimeUtil.java
@@ -14,27 +14,66 @@ import org.joda.time.format.DateTimeFormatter;
 public class DateTimeUtil {
 	public final static String pattern = "yyyy-MM-dd hh:mm:ss";
 	/**
-	 * 当前时间
+	 * 当前时间 yyyy-MM-dd hh:mm:ss
 	 * @param timezone
 	 * @return
 	 */
-	public static String curDate(String timezone){
-		//DateTimeZone zone = DateTimeZone.forID("Asia/Tokyo");
+	public static String curDateTime(String timezone){
 		DateTimeZone zone = DateTimeZone.forID(timezone);
 		Chronology gregorianJuian = GJChronology.getInstance(zone);
 		DateTime dt = new DateTime(gregorianJuian);
 		DateTimeFormatter format = DateTimeFormat.forPattern(pattern);
 		return dt.toString(format);
 	}
+	
+	/**
+	 * 时间戳 转 时间 yyyy-MM-dd hh:mm:ss
+	 * @param millis
+	 * @param timezone
+	 * @return
+	 */
+	public static String toDateTime(long millis,String timezone){
+		DateTimeZone zone = DateTimeZone.forID(timezone);
+		Chronology gregorianJuian = GJChronology.getInstance(zone);
+		DateTime dt = new DateTime(millis,gregorianJuian);
+		DateTimeFormatter format = DateTimeFormat.forPattern(pattern);
+		return dt.toString(format);
+	}
+	
+	/**
+	 * 当前时间戳 毫秒
+	 * @param timezone
+	 * @return
+	 */
+	public static long getMillis(String timezone){
+		DateTimeZone zone = DateTimeZone.forID(timezone);
+		Chronology gregorianJuian = GJChronology.getInstance(zone);
+		DateTime dt = new DateTime(gregorianJuian);
+		return dt.getMillis();
+	}
+	/**
+	 * 当前时间戳 秒
+	 * @param timezone
+	 * @return
+	 */
+	public static int getSeconds(String timezone){
+		DateTimeZone zone = DateTimeZone.forID(timezone);
+		Chronology gregorianJuian = GJChronology.getInstance(zone);
+		DateTime dt = new DateTime(gregorianJuian);
+		return (int) (dt.getMillis()/1000);
+	}
+	
 	/**
 	 * 默认返回 Asia/Shanghai 时区时间
 	 * @return
 	 */
-	public static String curDate(){
-		return curDate("Asia/Shanghai");
+	public static String curDateTime(){
+		return curDateTime("Asia/Shanghai");
 	}
 	
+	
 	public static void main(String[] args) {
-		System.out.println(curDate());
+		//System.out.println(curDateTime());
+		System.out.println(toDateTime(getMillis("America/Dawson"),"America/Dawson"));
 	}
 }
