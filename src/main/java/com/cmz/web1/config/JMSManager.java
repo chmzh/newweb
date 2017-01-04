390a5e6d4cb6b4033aee11cca5b805a93cbc3fd6
chenmingzhou <sd@dfh.com>
2017-01-04 14:55:34 +0800

    jms

--- src/main/java/com/cmz/web1/config/JMSManager.java
+++ src/main/java/com/cmz/web1/config/JMSManager.java
@@ -1,17 +1,28 @@
 package com.cmz.web1.config;
 
 import javax.jms.ConnectionFactory;
+import javax.jms.MessageListener;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.listener.DefaultMessageListenerContainer;
+import org.springframework.jms.listener.MessageListenerContainer;
+import org.springframework.jms.listener.adapter.MessageListenerAdapter;
 
+import com.cmz.web1.jms.queue.ConsumerListener;
 import com.cmz.web1.jms.queue.ConsumerService;
 import com.cmz.web1.jms.queue.ConsumerServiceImpl;
+import com.cmz.web1.jms.queue.ConsumerSessionAwareMessageListener;
 import com.cmz.web1.jms.queue.ProducerService;
 import com.cmz.web1.jms.queue.ProducerServiceImpl;
+import com.cmz.web1.jms.queue.QueueMessageListener;
+import com.cmz.web1.jms.topic.TopicMessageListener;
+import com.cmz.web1.jms.topic.TopicMessageListener2;
+import com.cmz.web1.jms.topic.TopicProvider;
 
 @Configuration
 public class JMSManager {
@@ -25,6 +36,8 @@ public class JMSManager {
 		connectionFactory.setBrokerURL("failover:(tcp://localhost:61616)");
 		return connectionFactory;
 	}
+	
+	//========================生产者-消费者
 	/**
 	 * 定义消息队列（Queue）
 	 */
@@ -63,7 +76,8 @@ public class JMSManager {
 		consumerService.setJmsTemplate(jmsTemplate());
 		return consumerService;
 	}
-	
+	//========================生产者-消费者
+	//========================发布-订阅
 	
 	/**
 	 * 定义消息队列（Queue）
@@ -72,4 +86,171 @@ public class JMSManager {
 	public ActiveMQQueue queueDestination2(){
 		return new ActiveMQQueue("queue2");
 	}
+	/**
+	 * 消息队列监听者（Queue)
+	 * @return
+	 */
+	@Bean
+	public MessageListener queueMessageListener(){
+		QueueMessageListener messageListener = new QueueMessageListener();
+		
+		return messageListener;
+	}
+	/**
+	 * 消息监听容器（Queue）
+	 * @return
+	 */
+	@Bean
+	public MessageListenerContainer messageListenerContainer(){
+		DefaultMessageListenerContainer messageListenerContainer = new DefaultMessageListenerContainer();
+		messageListenerContainer.setConnectionFactory(connectionFactory());
+		messageListenerContainer.setDestination(queueDestination2());
+		messageListenerContainer.setMessageListener(queueMessageListener());
+		return messageListenerContainer;
+	}
+	/**
+	 * 定义消息主题（Topic）
+	 * @return
+	 */
+	@Bean("topicDestination")
+	public ActiveMQTopic topicDestination(){
+		ActiveMQTopic topicDestination = new ActiveMQTopic("my-topic");
+		
+		return topicDestination;
+	}
+	/**
+	 * 配置JMS模板（Topic）
+	 * @return
+	 */
+	@Bean
+	public JmsTemplate topicJmsTemplate(){
+		JmsTemplate jmsTemplate = new JmsTemplate();
+		jmsTemplate.setConnectionFactory(connectionFactory());
+		jmsTemplate.setDefaultDestination(topicDestination());
+		jmsTemplate.setPubSubDomain(true);
+		jmsTemplate.setReceiveTimeout(10000);
+		return jmsTemplate;
+	}
+	/**
+	 * topic消息发布者
+	 * @return
+	 */
+	@Bean
+	public TopicProvider topicProvider(){
+		TopicProvider topicProvider = new TopicProvider();
+		topicProvider.setTopicJmsTemplate(topicJmsTemplate());
+		return topicProvider;
+	}
+	/**
+	 * 消息主题监听者(Topic)
+	 * @return
+	 */
+	public TopicMessageListener topicMessageListener(){
+		return new TopicMessageListener();
+	}
+	
+	/**
+	 * 消息主题监听者(Topic)
+	 * @return
+	 */
+	public TopicMessageListener2 topicMessageListener2(){
+		return new TopicMessageListener2();
+	}
+	
+	/**
+	 * 主题监听容器 （Topic)
+	 * @return
+	 */
+	@Bean("topicJmsContainer")
+	public DefaultMessageListenerContainer defaultMessageListenerContainer(){
+		DefaultMessageListenerContainer defaultMessageListenerContainer = new DefaultMessageListenerContainer();
+		defaultMessageListenerContainer.setConnectionFactory(connectionFactory());
+		defaultMessageListenerContainer.setDestination(topicDestination());
+		defaultMessageListenerContainer.setMessageListener(topicMessageListener());
+		return defaultMessageListenerContainer;
+	}
+	/**
+	 * 主题监听容器 （Topic)
+	 * @return
+	 */
+	@Bean("topicJmsContainer2")
+	public DefaultMessageListenerContainer defaultMessageListenerContainer2(){
+		DefaultMessageListenerContainer defaultMessageListenerContainer = new DefaultMessageListenerContainer();
+		defaultMessageListenerContainer.setConnectionFactory(connectionFactory());
+		defaultMessageListenerContainer.setDestination(topicDestination());
+		defaultMessageListenerContainer.setMessageListener(topicMessageListener2());
+		return defaultMessageListenerContainer;
+	}
+	//========================发布-订阅
+	
+	//========================sessionAware
+	/**
+	 * 这个是sessionAwareQueue目的地
+	 * @return
+	 */
+	@Bean("sessionAwareQueue")
+	public ActiveMQQueue sessionAwareQueue(){
+		return new ActiveMQQueue("sessionAwareQueue");
+	}
+	
+	/**
+	 * 可以获取session的MessageListener
+	 * @return
+	 */
+	@Bean
+	public ConsumerSessionAwareMessageListener consumerSessionAwareMessageListener(){
+		ConsumerSessionAwareMessageListener consumerSessionAwareMessageListener = new ConsumerSessionAwareMessageListener();
+		consumerSessionAwareMessageListener.setDestination(queueDestination());
+		return consumerSessionAwareMessageListener;
+	}
+	
+	/**
+	 * 监听sessionAwareQueue 队列的消息，把回复消息写入 queueDestination指向队列，即queue1
+	 * @return
+	 */
+	@Bean
+	public DefaultMessageListenerContainer sessionAwareListenerContainer(){
+		DefaultMessageListenerContainer defaultMessageListenerContainer = new DefaultMessageListenerContainer();
+		defaultMessageListenerContainer.setConnectionFactory(connectionFactory());
+		defaultMessageListenerContainer.setDestination(sessionAwareQueue());
+		defaultMessageListenerContainer.setMessageListener(consumerSessionAwareMessageListener());
+		return defaultMessageListenerContainer;
+	}
+	
+	//========================sessionAware
+	
+	
+	//========================adapter
+	/**
+	 * 这个是adapterQueue目的地
+	 * @return
+	 */
+	@Bean("adapterQueue")
+	public ActiveMQQueue adapterQueue(){
+		return new ActiveMQQueue("adapterQueue");
+	}
+	/**
+	 * 消息监听适配器
+	 * @return
+	 */
+	@Bean
+	public MessageListenerAdapter messageListenerAdapter(){
+		MessageListenerAdapter adapter = new MessageListenerAdapter();
+		adapter.setDelegate(new ConsumerListener());
+		adapter.setDefaultListenerMethod("receiveMessage");
+		return adapter;
+	}
+	/**
+	 * 消息监听适配器对应的监听容器
+	 */
+	@Bean
+	public DefaultMessageListenerContainer messageListenerAdapterContainer(){
+		DefaultMessageListenerContainer defaultMessageListenerContainer = new DefaultMessageListenerContainer();
+		defaultMessageListenerContainer.setConnectionFactory(connectionFactory());
+		defaultMessageListenerContainer.setDestination(adapterQueue());
+		defaultMessageListenerContainer.setMessageListener(messageListenerAdapter());
+		return defaultMessageListenerContainer;
+	}
+	
+	//========================adapter
 }
--- src/main/java/com/cmz/web1/controller/JmsController.java
+++ src/main/java/com/cmz/web1/controller/JmsController.java
@@ -1,6 +1,8 @@
 package com.cmz.web1.controller;
 
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cmz.web1.jms.queue.ConsumerService;
 import com.cmz.web1.jms.queue.ProducerService;
+import com.cmz.web1.jms.topic.TopicProvider;
 
 @Controller
 @RequestMapping("/jms/")
@@ -20,17 +23,86 @@ public class JmsController {
 	@Autowired
 	@Qualifier("queueDestination1")
 	private ActiveMQQueue queueDestination;
-	
+	@Autowired
+	@Qualifier("sessionAwareQueue")
+	private ActiveMQQueue sessionAwareQueue;
+
+	@Autowired
+	@Qualifier("adapterQueue")
+	private ActiveMQQueue adapterQueue;
+
+	@Autowired
+	private TopicProvider topicProvider;
+	@Autowired
+	// @Qualifier("topicDestination")
+	private ActiveMQTopic topic;
+
+	/**
+	 * 测试生产者向queue1发送消息
+	 */
 	@RequestMapping("send")
 	@ResponseBody
-	public String send(String msg){
+	public String send(String msg) {
 		producerService.sendMessage(msg);
 		return msg;
 	}
-	
+
+	/**
+	 * 测试消费者从queue1接受消息
+	 */
 	@RequestMapping("rec")
 	@ResponseBody
-	public String rec(String msg){
+	public String rec(String msg) {
 		return consumerService.receive(queueDestination);
+	}
+
+	/**
+	 * 测试主题监听
+	 * 
+	 * 1.生产者向主题发布消息
+	 * 
+	 * 2.ConsumerMessageListener监听主题，并消费消息
+	 */
+	@RequestMapping("publish")
+	@ResponseBody
+	public String publish(String msg) {
+		topicProvider.publish(topic, msg);
+		return msg;
+	}
+
+	/**
+	 * 测试SessionAwareMessageListener
+	 * 
+	 * 1. 生产者向队列sessionAwareQueue发送消息
+	 * 
+	 * 2. SessionAwareMessageListener接受消息，并向queue1队列发送回复消息
+	 * 
+	 * 3. 消费者从queue1消费消息
+	 * 
+	 */
+	@RequestMapping("aware")
+	@ResponseBody
+	public String testAware() {
+		producerService.sendMessage(sessionAwareQueue, "Hello sessionAware");
+		consumerService.receive(queueDestination);
+		return null;
+	}
+
+	/**
+	 * 测试MessageListenerAdapter
+	 * 
+	 * 1. 生产者向队列adapterQueue发送消息
+	 * 
+	 * 2. MessageListenerAdapter使ConsumerListener接受消息，并向queue1队列发送回复消息
+	 * 
+	 * 3. 消费者从queue1消费消息
+	 * 
+	 */
+	@RequestMapping("adapter")
+	@ResponseBody
+	public String testAdapter() {
+		producerService.sendMessage(adapterQueue, "Hello adapterQueue", queueDestination);
+		consumerService.receive(queueDestination);
+		return null;
 	}
 }
