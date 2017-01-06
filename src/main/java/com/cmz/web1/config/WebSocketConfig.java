6aadf11ee9eb8fd7d050d97ca0c62a9e7ad243f6
chenmingzhou <sd@dfh.com>
2017-01-06 09:44:21 +0800

    websocket

--- src/main/java/com/cmz/web1/config/WebSocketConfig.java
+++ src/main/java/com/cmz/web1/config/WebSocketConfig.java
@@ -1,40 +1,31 @@
 package com.cmz.web1.config;
 
+import javax.annotation.Resource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-import com.cmz.web1.handler.SystemWebSocketHandler;
+import com.cmz.web1.handler.HandShake;
+import com.cmz.web1.handler.MyWebSocketHandler;
 
-@Configuration  
-@EnableWebMvc  
-@EnableWebSocket  
-public class WebSocketConfig{// extends WebMvcConfigurerAdapter implements WebSocketConfigurer{  
-  /*
-    @Override  
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {  
-        registry.addHandler(systemWebSocketHandler(),"/webSocketServer");  
-        registry.addHandler(systemWebSocketHandler(),"/webSocketServer/sockjs").setAllowedOrigins("*").withSockJS();  
-         //registry.addHandler(systemWebSocketHandler(),"/webSocketServer").addInterceptors(new WebSocketHandshakeInterceptor());  
-         //registry.addHandler(systemWebSocketHandler(), "/sockjs/webSocketServer").addInterceptors(new WebSocketHandshakeInterceptor()).withSockJS();  
-         //registry.addHandler(systemWebSocketHandler(), "/webSocketServer/sockjs").withSockJS();  
-         //registry.addHandler(systemWebSocketHandler(),"/ws").addInterceptors(new WebSocketHandshakeInterceptor()); 
-         //   registry.addHandler(systemWebSocketHandler(), "/ws/sockjs").addInterceptors(new WebSocketHandshakeInterceptor()) 
-         //           .withSockJS();
-    }  
-      
-    @Bean  
-    public WebSocketHandler systemWebSocketHandler(){  
-        return new SystemWebSocketHandler();  
-    }  
-    */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+
+	@Bean
+	public MyWebSocketHandler handler(){
+		return new MyWebSocketHandler();
+	}
+
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(handler(), "/ws").addInterceptors(new HandShake());
+
+		registry.addHandler(handler(), "/ws/sockjs").addInterceptors(new HandShake()).withSockJS();
+	}
+
 }
